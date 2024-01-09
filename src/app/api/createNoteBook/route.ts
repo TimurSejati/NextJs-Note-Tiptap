2978de327b15eec71663ee13bdb96939bcf15787
Timur Dian Radha Sejati <timur7dian84@gmail.com>
2024-01-09 21:45:50 +0700

    [update]

---
+++ src/app/api/createNoteBook/route.ts
@@ -0,0 +1,46 @@
+// /api/createNoteBook
+
+import { db } from "@/lib/db";
+import { $notes } from "@/lib/db/schema";
+import { generateImage, generateImagePrompt } from "@/lib/openai";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  const { userId } = auth();
+  if (!userId) {
+    return new NextResponse("unauthorised", { status: 401 });
+  }
+  const body = await req.json();
+  const { name } = body;
+  // const image_description = await generateImagePrompt(name);
+  // if (!image_description) {
+  //   return new NextResponse("failed to generate image description", {
+  //     status: 500,
+  //   });
+  // }
+  // const image_url = await generateImage(image_description);
+  // if (!image_url) {
+  //   return new NextResponse("failed to generate image ", {
+  //     status: 500,
+  //   });
+  // }
+
+  const note_ids = await db
+    .insert($notes)
+    .values({
+      name,
+      userId,
+      imageUrl:
+        "https://img.freepik.com/free-vector/yellow-note-paper-with-red-pin_1284-42430.jpg",
+    })
+    .returning({
+      insertedId: $notes.id,
+    });
+
+  return NextResponse.json({
+    note_id: note_ids[0].insertedId,
+  });
+}
